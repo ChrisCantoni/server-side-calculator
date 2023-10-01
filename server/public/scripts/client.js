aa93d63728460b4d32c6e0f04ea52218b777dcec
Chris Cantoni <chris.cantoni@gmail.com>
2023-10-01 10:43:29 -0500

    Data sent to and calculated on server.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,9 @@
 console.log('client.js sourced');
 
 let answerDisplay = document.querySelector('#answer-display');
+let typeOfMath = '';
+const buttonColor = document.getElementsByClassName('typeOfMath');
+console.log(calculator);
 
 function runNumbers(event) {
     event.preventDefault();
@@ -8,10 +11,48 @@ function runNumbers(event) {
     firstNumber = Number(firstNumber);
     let secondNumber = document.querySelector('#second-number').value;
     secondNumber = Number(secondNumber);
-    finalAnswer = firstNumber + secondNumber;
-    answerDisplay.innerHTML += `
-        <tr>
-            <td>${firstNumber} + ${secondNumber}</td>
-            <td>= ${finalAnswer}</td>
-    `
+    //let finalAnswer = 0;
+    console.log(`${firstNumber} ${typeOfMath} ${secondNumber}`);
+    axios.post('/calculate', {
+        firstNumber: firstNumber,
+        typeOfMath: typeOfMath,
+        secondNumber: secondNumber
+    }).then((response) => {
+        console.log('Calculation sent to server');
+    }).catch((error) => {
+        console.error(error);
+        alert('Something went wrong');
+    })
+    // if (typeOfMath == '+') {
+    //     finalAnswer = firstNumber + secondNumber;
+        
+    // } else if (typeOfMath == '-') {
+    //     finalAnswer = firstNumber - secondNumber;
+    // }
+        
+    //     else {
+    //         console.log('Something went wrong');
+    //     }
+    //     answerDisplay.innerHTML += `
+    //         <tr>
+    //             <td>${firstNumber} ${typeOfMath} ${secondNumber}</td>
+    //             <td>= ${finalAnswer}</td>`
+}
+
+function addFunction(event) {
+    typeOfMath = '+';
+    clearFunctions(); // clear every other color if you change it
+    event.target.style.backgroundColor = 'yellow';
+}
+
+function clearFunctions() {
+    for (let button of buttonColor) {
+        button.style.backgroundColor = 'white';
+    }
+}
+
+function subtractFunction(event) {
+    typeOfMath = '-';
+    clearFunctions();
+    event.target.style.backgroundColor = 'yellow';
 }
--- server/server.js
+++ server/server.js
@@ -3,4 +3,35 @@ const app = express();
 const PORT = 5003;
 
 app.use(express.json());
-app.use(express.static('./server/public'));
+app.use(express.static('server/public'));
+
+const calculator = [];
+
+
+
+app.post('/calculate', (req, res) => {
+    console.log(req.body);
+    let newCalc = req.body;
+    getAnswer(newCalc);
+    res.sendStatus(201);
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
+
+function getAnswer(newCalc) {
+    if (newCalc.typeOfMath == '+') {
+        newCalc.finalAnswer = newCalc.firstNumber + newCalc.secondNumber;
+    } else if (newCalc.typeOfMath == '-') {
+        newCalc.finalAnswer = newCalc.firstNumber - newCalc.secondNumber;
+    }
+        
+    else {
+        console.log('Something went wrong');
+    }
+    calculator.push(newCalc);
+    console.log(calculator);
+}
